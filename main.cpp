b7607a3adb4df0b8b870386a53a8ccc1e4ad2a5e
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-04-21 13:43:40 -0700

    add constant parameters

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <omp.h>
 
-
+// Globals
 int	NowYear;		// 2023 - 2028
 int	NowMonth;		// 0 - 11
 
@@ -13,6 +13,23 @@ float	NowPrecip;		// inches of rain per month
 float	NowTemp;		// temperature this month
 float	NowHeight;		// rye grass height in inches
 int	NowNumRabbits;		// number of rabbits in the current population
+
+
+// Coefficients for global changes over time.
+const float RYEGRASS_GROWS_PER_MONTH =		20.0;
+const float ONE_RABBITS_EATS_PER_MONTH =	 1.0;
+
+const float AVG_PRECIP_PER_MONTH =	       12.0;	// average
+const float AMP_PRECIP_PER_MONTH =		4.0;	// plus or minus
+const float RANDOM_PRECIP =			2.0;	// plus or minus noise
+
+const float AVG_TEMP =				60.0;	// average
+const float AMP_TEMP =				20.0;	// plus or minus
+const float RANDOM_TEMP =			10.0;	// plus or minus noise
+
+const float MIDTEMP =				60.0;
+const float MIDPRECIP =				14.0;
+
 
 int main( int argc, char *argv[ ] )
 {
